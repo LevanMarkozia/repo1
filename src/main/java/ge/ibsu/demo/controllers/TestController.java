05c76cce69d0ef51316ed35ca1a0aa60e2d7fd02
Levan Markozia <23202242@ibsu.edu.ge>
2024-11-26 11:00:12 +0400

    First commit

--- src/main/java/ge/ibsu/demo/controllers/TestController.java
+++ src/main/java/ge/ibsu/demo/controllers/TestController.java
@@ -1,2 +1,21 @@
-package ge.ibsu.demo.controllers;public class TestController {
+package ge.ibsu.demo.controllers;
+
+import ge.ibsu.demo.dto.Person;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @RequestMapping(value="/hello",method= RequestMethod.GET,produces={"application/json"})
+    public String hello(){
+        return "Hi";
+    }
+    @RequestMapping(value="/person",method=RequestMethod.PATCH,produces={"application/json"})
+    public Person testPost(@RequestBody Person p){
+        p.setName(p.getName()+" from server");
+        return p;
+    }
 }
--- src/main/java/ge/ibsu/demo/dto/Person.java
+++ src/main/java/ge/ibsu/demo/dto/Person.java
@@ -1,2 +1,13 @@
-package ge.ibsu.demo.dto;public class Person {
+package ge.ibsu.demo.dto;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
